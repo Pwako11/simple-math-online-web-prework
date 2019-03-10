fd37d5861461e2bd563f5b24e29be4236560cf79
Patrick A <akolo@hotmail.com>
2019-03-10 14:23:43 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,6 +9,7 @@ num8 = 8
 num9 = 9
 
 def addition(num1, num2)
+<<<<<<< HEAD
    
 	num1+num2
   
@@ -42,6 +43,63 @@ def square_root(num9)
 
 	Math.sqrt(num9)
   
+=======
+  num1 = 1 
+  num2 = 2 
+  num2 + num2
+  puts addition
+  addition
+  
+end
+
+def subtraction(num1, num2)
+  num1 = 1 
+  num2 = 2 
+  num2 - num2
+  puts subtraction
+  subtraction
+  
+end
+
+def division(num1, num2)
+  num1 = 1 
+  num2 = 2 
+  num2 / num2
+  puts division
+  division
+end
+
+def multiplication(num1, num2)
+  num1 = 1 
+  num2 = 2 
+  num2 * num2
+  puts multiplication
+  multiplication
+end
+
+def modulo(num1, num2)
+  num1 = 5
+  num2 = 2 
+  num2 % num2
+  puts modulo
+  modulo
+end
+
+def square_root(num)
+  num = 9
+  math.sqrt(num)
+  square_root
+end
+
+def order_of_operation(num1, num2, num3, num4)
+  num1 = 1 
+  num2 = 2 
+  num3 = 3 
+  num4 = 4 
+  num1 + ((num4 * num3)/num2)
+  order_of_operation
+  #Hint:  __ + (( __ * __ ) / __ )
+>>>>>>> 35204d7e384b0e1725cdfa44cabd7dd30d1f6199
 end
 
 def order_of_operation(num1, num4, num3, num2)
